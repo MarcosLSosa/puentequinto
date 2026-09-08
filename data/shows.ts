d90057725c0aca080f2a11d6cb9192012f397c21
marcos sosa <marcoslucianososa@gmail.com>
2026-09-08 11:03:59 -0300

    mejoras en show

--- data/shows.ts
+++ data/shows.ts
@@ -1,17 +1,22 @@
 export const shows = [
   {
-    date: "15 AGO",
-    city: "SAN LUIS",
-    place: "CASINO NEW YORK"
+    date: "07 AGO · 23:00 HS",
+    city: "VILLA MERCEDES",
+    place: "BOLICHE DON MIRANDA",
+    status: "Último show",
+    flyer: "/images/flyer_puente_quinto_bolichelosmirandas.png",
+    map: "https://maps.app.goo.gl/9E5Skh7cM6tEuE2E7"
   },
   {
     date: "22 AGO",
     city: "VILLA MERCEDES",
-    place: "EVENTO PRIVADO"
+    place: "EVENTO PRIVADO",
+    status: "Show anterior"
   },
   {
-    date: "05 SEP",
-    city: "MENDOZA",
-    place: "PRÓXIMAMENTE"
+    date: "15 AGO",
+    city: "SAN LUIS",
+    place: "CASINO NEW YORK",
+    status: "Show anterior"
   }
 ];
